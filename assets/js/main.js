6f9c2d377890783d3b24d13e68cef1e654ae37cb
Mateus Gonçalves <mflgcontato@gmail.com>
2026-06-01 13:03:14 -0300

    Aplica gitignore

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initNavbarEvents } from './components/navbar.js';
-import { initModalEvents } from './components/modal.js';
-
-console.log("main.js carregado");
-
-document.addEventListener('DOMContentLoaded', () => {
-  initNavbarEvents();
-  initModalEvents();
-});
